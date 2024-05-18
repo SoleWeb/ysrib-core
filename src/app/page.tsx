84f81049a4885650ac357114feeee92f52a9ee55
Mohd Danish <mohddanish8299@gmail.com>
2024-05-18 12:04:28 +0530

    added landing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./navbar/page";
+import LandingSection from "./landing-section/page";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <LandingSection />
     </div>
   );
 }
