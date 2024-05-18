48e443d20ea93e5791b085aad94ab11c621f4bd6
Mohd Danish <mohddanish8299@gmail.com>
2024-05-18 12:48:24 +0530

    added key features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Navbar from "./navbar/page";
 import LandingSection from "./landing-section/page";
+import Carousel from "../components/carousel";
+import KeyFeatures from "./brands-section/page";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <LandingSection />
+      <Carousel />
+      <KeyFeatures />
     </div>
   );
 }
