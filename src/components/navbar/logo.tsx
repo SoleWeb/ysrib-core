cf86edc8854732721d14f0baa1eddc5baed744bb
Mohd Danish <mohddanish8299@gmail.com>
2024-05-24 06:41:08 +0530

    fixed logo for dar/light screens

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -8,17 +8,19 @@ interface BrandLogoProps {
 
 const BrandLogo = ({ className }: BrandLogoProps) => {
   return (
-    <Link href="./" className={className}>
-      <div className="pl-6 w-fit h-20 flex justify-center items-center">
+      <div className="w-fit h-20 flex justify-center items-center">
+      <Link href="./" className={className}>
+        <div className="p-2 bg-foreground/10 dark:bg-foreground/[.05] rounded-lg">
         <Image
           src="/images/favicon_io/logo.png"
           alt="Yasrib brand logo"
           width={40}
           height={40}
-          className="dark:invert"
+          style={{filter: 'brightness(0) saturate(100%) invert(12%) sepia(100%) saturate(6557%) hue-rotate(269deg) brightness(92%) contrast(123%)'}}
         />
+        </div>
+        </Link>
       </div>
-    </Link>
   );
 };
 
